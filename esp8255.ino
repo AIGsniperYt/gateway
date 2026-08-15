963de99f8ac21eed8ba732a39c29b87f8a638063
AIGsniperYt <thahmimmustaq@gmail.com>
2026-08-15 21:43:22 +0100

    improve sync logic by emptying buffers, replace if with while because fundamentally, while = if loop but iterated, not ran once

--- esp8255.ino
+++ esp8255.ino
@@ -9,23 +9,30 @@ SoftwareSerial sim800(4, 5); // RX=D2, TX=D1
 
 // this function sets up once when the esp boots
 void setup() {
-  Serial.begin(115200); // nice and obvious, set baud rate to this for serial monitor from esp
-  sim800.begin(9600); // use this baud rate to talk to the sim800
+    // set baud rates here
+    Serial.begin(115200); // nice and obvious, set baud rate to this for serial monitor from esp
+    sim800.begin(9600); // use this baud rate to talk to the sim800
 }
 
 void sync_with_sim() {
     // is there data available from the serial monitor on your computer?
     // (basically, did you type and submit a command?)
-    if (Serial.available()) {
+    while (Serial.available()) {
         // send available data to the sim after reading it
         sim800.write(Serial.read());
     }
 
     // did sim send any data back?
-    if (sim800.available()) {
+    while (sim800.available()) {
         // write incoming data to serial monitor
         Serial.write(sim800.read());
     }
+
+    // if statements were replaced by while 
+    // this is because read only reads ONE byte
+    // problem: >1 byte responses could be missed
+    // solution: while is an infinite if statement anyways,
+    // replace if with while: if there is data, read until there is none left to read
 }
 
 // this is the function that runs continously
