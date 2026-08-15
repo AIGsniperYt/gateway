61971641a43adbd36e0dfea75ac2e2421836a3ca
AIGsniperYt <thahmimmustaq@gmail.com>
2026-08-15 22:19:35 +0100

    make a helper function api to send commands within the code, and test it via calling AT with it initially on boot

--- esp8255.ino
+++ esp8255.ino
@@ -2,17 +2,10 @@
 // to leave the hardware default ones open for serial monitor 
 // basically enable fake serial ports so we can talk to sim800 on other pins
 
-
 // create a softwareserial object called sim800, this tells the esp8266:
 // listen for incoming sim800 data on gpio4, and send outgoing to gpio5
 SoftwareSerial sim800(4, 5); // RX=D2, TX=D1
 
-// this function sets up once when the esp boots
-void setup() {
-    // set baud rates here
-    Serial.begin(115200); // nice and obvious, set baud rate to this for serial monitor from esp
-    sim800.begin(9600); // use this baud rate to talk to the sim800
-}
 
 void sync_with_sim() {
     // is there data available from the serial monitor on your computer?
@@ -34,6 +27,37 @@ void sync_with_sim() {
     // solution: while is an infinite if statement anyways,
     // replace if with while: if there is data, read until there is none left to read
 }
+
+// this is a backend function meant to be hit as an api
+// it will return a response as a string as you can see
+String send_cmd(String cmd, uint16_t timeout = 2000) {
+    String response = ""; // build the response string byte by byte
+    // cus remember, .read() only does one byte at a time
+    // so we need somewhere to store this data while its being build
+
+    sim800.println(cmd);   // send the command
+    unsigned long start = millis(); // basically in short, store current time
+
+    // wait for response boilerplate logic
+    while (millis() - start < timeout) { // ensure ALL data is read, not one byte with a naive if statement
+        while (sim800.available()) {
+            response += (char)sim800.read();
+        }
+        yield(); // keep ESP8266 alive, or watchdog timer resets chip apparently
+    }
+
+    return response;
+}
+
+
+// this function sets up once when the esp boots
+void setup() {
+    // set baud rates here
+    Serial.begin(115200); // nice and obvious, set baud rate to this for serial monitor from esp
+    sim800.begin(9600); // use this baud rate to talk to the sim800
+    send_cmd("AT");
+}
+
 
 // this is the function that runs continously
 void loop() {
